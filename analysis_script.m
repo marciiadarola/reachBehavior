526fef3eba6e2c49af8b0b537cdc71b8a225219b
kimerein <kimerein@gmail.com>
2017-08-24 15:55:03 -0400

    Concatenate trial-by-trial data across movies

--- analysis_script.m
+++ analysis_script.m
@@ -1,7 +1,8 @@
 %% Which experiment to analyze
-nameOfMicroSD='C:\Users\Kim\Documents\MATLAB\Final analysis\20170503\OUTPUT.txt';
-nameOfVideoFile='C:\Users\Kim\Documents\MATLAB\Final analysis\20170503\2017-04-14 07-14-06-C.avi';
-control=true; % if this is a control where pellet not loaded every time
+nameOfMicroSD='C:\Users\Kim\Documents\MATLAB\Final analysis\bl_3_1 20170510\OUTPUT.txt';
+nameOfVideoFile='C:\Users\Kim\Documents\MATLAB\Final analysis\bl_3_1 20170510\1980-05-19 10-40-26-C.avi';
+control=false; % if this is a control where pellet not loaded every time
+isInSecondHalf=0; % if this movie is in the second half of arduino output file
 
 %% Get data from Arduino output file        
 endoffname=regexp(nameOfMicroSD,'\');            
@@ -13,7 +14,7 @@ a=load([nameOfVideoFile(1:endoffname(end)-1) '_savehandles.mat']);
 savehandles=a.savehandles;
 
 %% Do alignment of Arduino and movie data
-aligned=getAlignment(out,30,savehandles);
+aligned=getAlignment(out,30,savehandles,isInSecondHalf);
 
 %% Save integrated data
 endofVfname=regexp(nameOfVideoFile,'\.');      
@@ -22,3 +23,4 @@ finaldata=integrateSDoutWithReaches(savehandles,out,30,aligned,[nameOfVideoFile(
 
 %% Make data figures
 tbt=plotCueTriggeredBehavior(finaldata,'cue',1);
+save([nameOfVideoFile(1:endofVfname(end)-1) '_processed_data\tbt.mat'],tbt);
---
+++ combineExptPieces.m
@@ -0,0 +1,54 @@
+function alltbt=combineExptPieces(expt_dir)
+
+ls=dir(expt_dir);
+j=1;
+tbt=[];
+alltbt=[];
+for i=1:length(ls)
+    thisname=ls(i).name;
+    thisisdir=ls(i).isdir;
+    if ~isempty(regexp(thisname,'processed_data')) && thisisdir==1
+        a=load([expt_dir '\' thisname '\tbt.mat']);
+        tbt{j}=a.tbt;
+        j=j+1;
+    end
+end
+
+if isempty(tbt)
+    disp('No tbt data saved in this directory');
+    return
+end
+
+f=fieldnames(tbt{1});
+for i=1:length(f)
+    alltbt.(f{i})=[];
+end
+
+for i=1:length(tbt)
+    curr_tbt=tbt{i};
+    if i==1
+        % first tbt
+        for j=1:length(f)
+            alltbt.(f{j})=curr_tbt.(f{j});
+        end
+    else
+        if size(alltbt.(f{1}),2)<size(curr_tbt.(f{1}),2)
+            % expand size, fill with nans
+            expandBy=size(curr_tbt.(f{1}),2)-size(alltbt.(f{1}),2);
+            for j=1:length(f)
+                alltbt.(f{j})=[alltbt.(f{j}) nan(size(alltbt.(f{j}),1),expandBy)];
+            end
+        elseif size(alltbt.(f{1}),2)>size(curr_tbt.(f{1}),2)
+            expandBy=size(alltbt.(f{1}),2)-size(curr_tbt.(f{1}),2);
+            for j=1:length(f)
+                curr_tbt.(f{j})=[curr_tbt.(f{j}) nan(size(curr_tbt.(f{j}),1),expandBy)];
+            end
+        end
+        % concat
+        for j=1:length(f)
+            alltbt.(f{j})=[alltbt.(f{j}); curr_tbt.(f{j})];
+        end
+    end
+end
+            
+        
