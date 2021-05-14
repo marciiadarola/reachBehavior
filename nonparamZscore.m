e6e0054405b3cc630650a411302f742de10f53c4
marciiadarola <marciiadarola@gmail.com>
2021-05-14 10:36:35 -0400

    MATLAB 2020b UPDATE
    omitnan syntax error

--- nonparamZscore.m
+++ nonparamZscore.m
@@ -2,6 +2,6 @@ function out=nonparamZscore(data)
 
 % data is a vector
 
-med=nanmedian(data);
-mad=nanmedian(abs(data-med));
+med=median(data,'omitnan');
+mad=median(abs(data-med),'omitnan');
 out=abs(data-med)./(1.4826*mad);
